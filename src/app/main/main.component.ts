da8ffdf4ec174e6ea9c66fe17c7e300dc5c9e0d0
Oleh Kosach <olezhkakosach@gmail.com>
2020-03-25 22:32:12 +0200

    changes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,3 +1,235 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-main',
+//   templateUrl: './main.component.html',
+//   styleUrls: ['./main.component.scss']
+// })
+// export class MainComponent implements OnInit {
+
+//   answers = [0, 0, 0];
+
+//   firstA: boolean = true;
+//   firstB: boolean = true;
+//   secondA: boolean = false;
+//   secondB: boolean = false;
+//   thirdA: boolean = false;
+//   thirdB: boolean = false;
+//   progressBarA;
+//   progressBarB;
+//   progressBarC;
+
+//   elementA;
+//   elementB;
+//   elementC;
+//   elementD;
+
+//   submit;
+//   radA;
+//   radB;
+//   radC;
+//   radD;
+
+//   constructor() { }
+
+//   SubmitQuestion() {
+
+//     this.elementA = document.getElementById("radioA") as HTMLInputElement;
+//     this.elementB = document.getElementById("radioB") as HTMLInputElement;
+//     this.elementC = document.getElementById("radioC") as HTMLInputElement;
+//     this.submit = document.getElementById("submit") as HTMLInputElement;
+//     this.radA = document.getElementsByClassName("radA")[0] as HTMLElement;
+//     this.radB = document.getElementsByClassName("radB")[0] as HTMLElement;
+//     this.radC = document.getElementsByClassName("radC")[0] as HTMLElement;
+
+//     this.progressBarA = document.getElementsByClassName("progressA")[0] as HTMLInputElement;
+//     this.progressBarB = document.getElementsByClassName("progressB")[0] as HTMLInputElement;
+//     this.progressBarC = document.getElementsByClassName("progressC")[0] as HTMLInputElement;
+
+//     if (this.elementA.checked !== false || this.elementB.checked !== false || this.elementC.checked !== false) {
+//       this.radA.style.color = "green";
+//       this.radB.style.color = "red";
+//       this.radC.style.color = "red";
+//       this.progressBarA.style.backgroundColor = "red";
+//     }
+
+//     console.log(this.elementA.checked, 'elem');
+
+//     if (this.elementA.checked == true) {
+//       this.progressBarA.style.backgroundColor = "green";
+//     }
+
+//     // if (this.elementA.checked !== true) {
+//     //   this.progressBarA.style.backgroundColor = "red";
+
+//     // } 
+//     // else {
+//     //   this.progressBarA.style.backgroundColor = "green";
+
+//     // }
+
+//   }
+
+//   NextQuestion() {
+//     this.elementA = document.getElementById("radioA") as HTMLInputElement;
+//     this.elementB = document.getElementById("radioB") as HTMLInputElement;
+//     this.elementC = document.getElementById("radioC") as HTMLInputElement;
+//     this.progressBarA = document.getElementsByClassName("progressA")[0] as HTMLInputElement;
+
+//     if (this.elementA.checked !== true) {
+//       this.progressBarA.style.backgroundColor = "red";
+//     }
+
+//     if (this.elementA.checked == false && this.elementB.checked == false && this.elementC.checked == false) {
+//       this.progressBarA.style.backgroundColor = "gray";
+//       console.log('gray1');
+//       this.answers[0] += 1;
+//     }
+
+//     // else {
+//     //   this.progressBarA.style.backgroundColor = "green";
+
+//     // }
+//     this.firstA = false;
+//     this.firstB = false;
+//     this.secondA = true;
+//     this.secondB = true;
+
+//   }
+
+//   SubmitQuestionTwo() {
+//     this.elementA = document.getElementById("radioA") as HTMLInputElement;
+//     this.elementB = document.getElementById("radioB") as HTMLInputElement;
+//     this.elementC = document.getElementById("radioC") as HTMLInputElement;
+//     this.elementD = document.getElementById("radioD") as HTMLInputElement;
+
+//     this.radD = document.getElementsByClassName("radD")[0] as HTMLElement;
+//     this.progressBarB = document.getElementsByClassName("progressB")[0] as HTMLInputElement;
+
+//     if (this.elementA.checked !== false || this.elementB.checked !== false || this.elementC.checked !== false || this.elementD.checked !== false) {
+//       this.radD.style.border = "4px solid green";
+//       this.radD.style.borderRadius = "12px";
+
+//     }
+
+//     if (this.elementD.checked !== true) {
+//       this.progressBarB.style.backgroundColor = "red";
+
+//     }
+//     // else {
+//     //   this.progressBarB.style.backgroundColor = "green";
+
+//     // }
+
+
+//   }
+
+//   NextQuestionTwo() {
+//     this.thirdA = true;
+//     this.thirdB = true;
+//     this.secondA = false;
+//     this.secondB = false;
+
+//     this.elementA = document.getElementById("radioA") as HTMLInputElement;
+//     this.elementB = document.getElementById("radioB") as HTMLInputElement;
+//     this.elementC = document.getElementById("radioC") as HTMLInputElement;
+//     this.elementD = document.getElementById("radioD") as HTMLInputElement;
+//     this.progressBarB = document.getElementsByClassName("progressB")[0] as HTMLInputElement;
+
+//     if (this.elementD.checked !== true) {
+//       this.progressBarB.style.backgroundColor = "red";
+//     }
+
+//     if (this.elementA.checked == false && this.elementB.checked == false && this.elementC.checked == false) {
+//       this.progressBarB.style.backgroundColor = "gray";
+//       this.answers[1] += 1;
+
+//     }
+
+
+//     // else {
+//     //   this.progressBarB.style.backgroundColor = "green";
+
+//     // }
+
+//   }
+
+//   SubmitQuestionThree() {
+//     this.elementA = document.getElementById("radioA") as HTMLInputElement;
+//     this.elementB = document.getElementById("radioB") as HTMLInputElement;
+//     this.elementC = document.getElementById("radioC") as HTMLInputElement;
+//     this.submit = document.getElementById("submit") as HTMLInputElement;
+//     this.radA = document.getElementsByClassName("radA")[0] as HTMLElement;
+//     this.radB = document.getElementsByClassName("radB")[0] as HTMLElement;
+//     this.radC = document.getElementsByClassName("radC")[0] as HTMLElement;
+
+//     this.progressBarA = document.getElementsByClassName("progressA")[0] as HTMLInputElement;
+//     this.progressBarB = document.getElementsByClassName("progressB")[0] as HTMLInputElement;
+//     this.progressBarC = document.getElementsByClassName("progressC")[0] as HTMLInputElement;
+
+//     if (this.elementA.checked !== false || this.elementB.checked !== false || this.elementC.checked !== false) {
+//       this.radA.style.color = "red";
+//       this.radB.style.color = "green";
+//       this.radC.style.color = "red";
+//       this.progressBarA.style.backgroundColor = "green";
+
+//     }
+
+//     if (this.elementB.checked !== true) {
+//       this.progressBarC.style.backgroundColor = "red";
+
+//     }
+//     // else {
+//     //   this.progressBarC.style.backgroundColor = "green";
+//     // }
+
+//   }
+
+//   NextQuestionThree() {
+//     this.elementA = document.getElementById("radioA") as HTMLInputElement;
+//     this.elementB = document.getElementById("radioB") as HTMLInputElement;
+//     this.elementC = document.getElementById("radioC") as HTMLInputElement;
+//     this.progressBarC = document.getElementsByClassName("progressC")[0] as HTMLInputElement;
+
+//     if (this.elementA.checked == false && this.elementB.checked == false && this.elementC.checked == false) {
+//       this.progressBarC.style.backgroundColor = "gray";
+//       this.answers[2] += 1;
+
+//     }
+
+//     if (this.elementB.checked !== true) {
+//       this.progressBarC.style.backgroundColor = "red";
+
+//     }
+
+//     for (let index = 0; index < this.answers.length; index++) {
+//       if (this.answers[index] > 0) {
+//         if (index = 0) {
+//           this.thirdA = false;
+//           this.thirdB = false;
+//           this.firstA = true;
+//           this.firstB = true;
+//         } else if (index = 1) {
+//           this.thirdA = false;
+//           this.thirdB = false;
+//           this.secondA = true;
+//           this.secondB = true;
+//         }
+//       }
+
+//     }
+//     // else {
+//     //   this.progressBarC.style.backgroundColor = "green";
+
+//     // }
+//   }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -15,6 +247,7 @@ export class MainComponent implements OnInit {
   secondB: boolean = false;
   thirdA: boolean = false;
   thirdB: boolean = false;
+  check: boolean = false;
   progressBarA;
   progressBarB;
   progressBarC;
@@ -31,6 +264,33 @@ export class MainComponent implements OnInit {
   radD;
 
   constructor() { }
+
+  toQuestionOne() {
+    this.firstA = true;
+    this.firstB = true;
+    this.secondA = false;
+    this.secondB = false;
+    this.thirdA = false;
+    this.thirdB = false;
+  }
+
+  toQuestionTwo() {
+    this.firstA = false;
+    this.firstB = false;
+    this.secondA = true;
+    this.secondB = true;
+    this.thirdA = false;
+    this.thirdB = false;
+  }
+
+  toQuestionThree() {
+    this.firstA = false;
+    this.firstB = false;
+    this.secondA = false;
+    this.secondB = false;
+    this.thirdA = true;
+    this.thirdB = true;
+  }
 
   SubmitQuestion() {
 
@@ -53,20 +313,9 @@ export class MainComponent implements OnInit {
       this.progressBarA.style.backgroundColor = "red";
     }
 
-    console.log(this.elementA.checked, 'elem');
-
     if (this.elementA.checked == true) {
       this.progressBarA.style.backgroundColor = "green";
     }
-
-    // if (this.elementA.checked !== true) {
-    //   this.progressBarA.style.backgroundColor = "red";
-
-    // } 
-    // else {
-    //   this.progressBarA.style.backgroundColor = "green";
-
-    // }
 
   }
 
@@ -82,18 +331,51 @@ export class MainComponent implements OnInit {
 
     if (this.elementA.checked == false && this.elementB.checked == false && this.elementC.checked == false) {
       this.progressBarA.style.backgroundColor = "gray";
-      console.log('gray1');
       this.answers[0] += 1;
+    } else {
+      this.answers[0] -= 1;
     }
 
-    // else {
-    //   this.progressBarA.style.backgroundColor = "green";
+    console.log(this.check, 'check');
 
-    // }
-    this.firstA = false;
-    this.firstB = false;
-    this.secondA = true;
-    this.secondB = true;
+    if (this.check === true) {
+      for (let index = 0; index < this.answers.length; index++) {
+        let a = index;
+        console.log(a, 'a');
+        if (this.answers[a] > 0) {
+          if (a === 0) {
+            this.thirdA = false;
+            this.thirdB = false;
+            this.firstA = true;
+            this.firstB = true;
+            console.log('first', a);
+            return
+
+          } else if (a === 1) {
+            this.thirdA = false;
+            this.thirdB = false;
+            this.secondA = true;
+            this.secondB = true;
+            this.firstA = false;
+            this.firstB = false;
+            console.log('second', a);
+            return
+          } else if (a === 2) {
+            this.thirdA = true;
+            this.thirdB = true;
+            this.firstA = false;
+            this.firstB = false;
+          }
+        }
+      }
+    } else {
+      console.log('sdfas');
+
+      this.firstA = false;
+      this.firstB = false;
+      this.secondA = true;
+      this.secondB = true;
+    }
 
   }
 
@@ -116,6 +398,10 @@ export class MainComponent implements OnInit {
       this.progressBarB.style.backgroundColor = "red";
 
     }
+
+    if (this.elementD.checked == true) {
+      this.progressBarB.style.backgroundColor = "green";
+    }
     // else {
     //   this.progressBarB.style.backgroundColor = "green";
 
@@ -125,10 +411,6 @@ export class MainComponent implements OnInit {
   }
 
   NextQuestionTwo() {
-    this.thirdA = true;
-    this.thirdB = true;
-    this.secondA = false;
-    this.secondB = false;
 
     this.elementA = document.getElementById("radioA") as HTMLInputElement;
     this.elementB = document.getElementById("radioB") as HTMLInputElement;
@@ -136,16 +418,64 @@ export class MainComponent implements OnInit {
     this.elementD = document.getElementById("radioD") as HTMLInputElement;
     this.progressBarB = document.getElementsByClassName("progressB")[0] as HTMLInputElement;
 
-    if (this.elementD.checked !== true) {
-      this.progressBarB.style.backgroundColor = "red";
-    }
-
-    if (this.elementA.checked == false && this.elementB.checked == false && this.elementC.checked == false) {
+    //  debugger
+    if (this.elementD.checked == true) {
+      this.progressBarB.style.backgroundColor = "green";
+      console.log('green');
+      this.answers[1] -= 1;
+    } else {
       this.progressBarB.style.backgroundColor = "gray";
+      console.log('gray2');
       this.answers[1] += 1;
-
     }
-    
+
+    if (this.elementD.checked !== true && this.elementA.checked == true || this.elementB.checked == true || this.elementC.checked == true) {
+      this.progressBarB.style.backgroundColor = "red";
+      console.log('red1');
+      this.answers[1] -= 2;
+    }
+    // if (this.elementA.checked == true || this.elementB.checked == true || this.elementC.checked == true){
+    //   this.answers[1] -= 1;
+    // }
+
+    if (this.check === true) {
+      console.log(this.answers, 'answers');
+
+      for (let index = 0; index < this.answers.length; index++) {
+        let a = index;
+        console.log(a, 'a');
+        if (this.answers[a] > 0) {
+          if (a === 0) {
+            this.thirdA = false;
+            this.thirdB = false;
+            this.firstA = true;
+            this.firstB = true;
+            console.log('first', a);
+            return
+
+          } else if (a === 1) {
+            this.thirdA = false;
+            this.thirdB = false;
+            this.secondA = true;
+            this.secondB = true;
+            this.firstA = false;
+            this.firstB = false;
+            console.log('second', a);
+            return
+          } else if (a === 2) {
+            this.thirdA = true;
+            this.thirdB = true;
+            this.secondA = false;
+            this.secondB = false;
+          }
+        }
+      }
+    } else {
+      this.thirdA = true;
+      this.thirdB = true;
+      this.secondA = false;
+      this.secondB = false;
+    }
 
     // else {
     //   this.progressBarB.style.backgroundColor = "green";
@@ -167,18 +497,28 @@ export class MainComponent implements OnInit {
     this.progressBarB = document.getElementsByClassName("progressB")[0] as HTMLInputElement;
     this.progressBarC = document.getElementsByClassName("progressC")[0] as HTMLInputElement;
 
-    if (this.elementA.checked !== false || this.elementB.checked !== false || this.elementC.checked !== false) {
-      this.radA.style.color = "red";
-      this.radB.style.color = "green";
-      this.radC.style.color = "red";
-      this.progressBarA.style.backgroundColor = "green";
+    // if (this.elementA.checked !== false || this.elementB.checked !== false || this.elementC.checked !== false) {
+    //   this.radA.style.color = "red";
+    //   this.radB.style.color = "green";
+    //   this.radC.style.color = "red";
+    //   this.progressBarA.style.backgroundColor = "green";
 
+    // }
+
+    // if (this.elementB.checked !== true) {
+    //   this.progressBarC.style.backgroundColor = "red";
+
+    // }
+
+    if (this.elementA.checked !== false || this.elementB.checked !== false || this.elementC.checked !== false || this.elementD.checked !== false) {
+      this.radB.style.color = "green";
+      this.radB.style.borderRadius = "12px";
     }
 
     if (this.elementB.checked !== true) {
       this.progressBarC.style.backgroundColor = "red";
-
     }
+
     // else {
     //   this.progressBarC.style.backgroundColor = "green";
     // }
@@ -186,42 +526,55 @@ export class MainComponent implements OnInit {
   }
 
   NextQuestionThree() {
+    this.check = true;
+    console.log('la');
+
     this.elementA = document.getElementById("radioA") as HTMLInputElement;
     this.elementB = document.getElementById("radioB") as HTMLInputElement;
     this.elementC = document.getElementById("radioC") as HTMLInputElement;
     this.progressBarC = document.getElementsByClassName("progressC")[0] as HTMLInputElement;
 
-    if (this.elementA.checked == false && this.elementB.checked == false && this.elementC.checked == false) {
-      this.progressBarC.style.backgroundColor = "gray";
-      this.answers[2] += 1;
-
-    }
-
+    console.log('answers', this.answers);
     if (this.elementB.checked !== true) {
       this.progressBarC.style.backgroundColor = "red";
+    }
 
+    if (this.elementA.checked == false && this.elementB.checked == false && this.elementC.checked == false) {
+      this.progressBarC.style.backgroundColor = "gray";
+      console.log('gray3');
+      this.answers[2] += 1;
+    } else {
+      this.answers[2] -= 1;
+    }
+
+    if (this.elementB.checked == true) {
+      this.progressBarC.style.backgroundColor = "green";
     }
 
     for (let index = 0; index < this.answers.length; index++) {
-      if (this.answers[index] > 0) {
-        if (index = 0) {
+      let a = index;
+      console.log(a, 'a');
+      if (this.answers[a] > 0) {
+        if (a === 0) {
           this.thirdA = false;
           this.thirdB = false;
           this.firstA = true;
           this.firstB = true;
-        } else if (index = 1) {
+          console.log('first', a);
+          return
+
+        } else if (a === 1) {
           this.thirdA = false;
           this.thirdB = false;
           this.secondA = true;
           this.secondB = true;
+          this.firstA = false;
+          this.firstB = false;
+          console.log('second', a);
+          return
         }
       }
-
     }
-    // else {
-    //   this.progressBarC.style.backgroundColor = "green";
-
-    // }
   }
 
   ngOnInit(): void {
